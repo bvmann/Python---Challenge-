9530596a1031ebbb629c10e7ffebf80c4b6dcc40
britt mann <brittvmann@gmail.com git config --global user.name bvmann>
2021-04-07 21:25:45 -0700

    printing nicely

--- PyPoll/pollmain.py.py
+++ PyPoll/pollmain.py.py
@@ -29,8 +29,8 @@ with open(poll_path) as pollcsv :
     for x in candidate_name :
         candidate2.append(x)
    
-    print(candidate_name)
-    print(candidate2[1])
+    #print(candidate_name)
+    #print(candidate2[1])
 
     for x in candidate: 
         if x==candidate2[0]:
@@ -41,16 +41,25 @@ with open(poll_path) as pollcsv :
             candidate2count+=1
         elif x==candidate2[3]:
             candidate3count+=1
-
-            
-    print(candidate0count)
-    print(candidate1count) 
-    print(candidate2count)
-    print(candidate3count)    
-    percentwonc0=round(candidate0count/votes_cast, 2)
-    print(percentwonc0)
-
-    print(f'{candidate2[1]}', " : ",(percentwonc0),((candidate1count)) )
-
     winner=(statistics.mode(candidate))
+    percentwonc0=round(candidate0count/votes_cast, 2)        
+    percentwonc1=round(candidate1count/votes_cast, 2)
+    percentwonc2=round(candidate2count/votes_cast, 2)
+    percentwonc3=round(candidate3count/votes_cast, 2)
+
+
+    #print(candidate0count)
+    #print(candidate1count) 
+    #print(candidate2count)
+    #print(candidate3count)    
+    
+    #print(percentwonc0)
+    print(f'Ballots cast : ',(votes_cast))
+    print('--------------------------------')
+    print(f'{candidate2[0]}', " : ",(percentwonc0),"(",(candidate0count),")" )
+    print(f'{candidate2[1]}', " : ",(percentwonc1),"(",(candidate1count),")" )
+    print(f'{candidate2[2]}', " : ",(percentwonc2),"(",(candidate2count),")" )
+    print(f'{candidate2[3]}', " : ",(percentwonc3),"(",(candidate3count),")" )
+
+    print('----------------------------------')
     print(f'Winner is : ',(winner))

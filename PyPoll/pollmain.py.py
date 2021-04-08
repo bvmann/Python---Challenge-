5d52eabcb07d72e6faa52a43d83f48466b18f195
britt mann <brittvmann@gmail.com git config --global user.name bvmann>
2021-04-07 20:40:05 -0700

    pollmain.py.py

--- PyPoll/pollmain.py.py
+++ PyPoll/pollmain.py.py
@@ -12,7 +12,9 @@ with open(poll_path) as pollcsv :
     candidate=[]
     candidate2=[]
     candidate0count=0
-   
+    candidate1count=0
+    candidate2count=0
+    candidate3count=0
     for row in csvreader : 
         #main.append(row)
         candidate.append(row[2])
@@ -20,7 +22,7 @@ with open(poll_path) as pollcsv :
     
     #print(len(main))
     #print(main[1])
-    print(len(candidate))
+    #print(len(candidate))
     #print(candidate)
     votes_cast=(len(candidate))
     candidate_name=(set(candidate))
@@ -32,10 +34,19 @@ with open(poll_path) as pollcsv :
 
     for x in candidate: 
         if x==candidate2[0]:
-            candidate0count+= 1 
+            candidate0count+=1
+        elif x==candidate2[1]:
+            candidate1count+=1
+        elif x==candidate2[2]:
+            candidate2count+=1
+        elif x==candidate2[3]:
+            candidate3count+=1
+
             
     print(candidate0count)
-            
+    print(candidate1count) 
+    print(candidate2count)
+    print(candidate3count)    
     percentwonc0=candidate0count/votes_cast
     print(percentwonc0)
 

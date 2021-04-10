8e7a70c3bd094cf565338e822dac269ca3fa4444
britt mann <brittvmann@gmail.com git config --global user.name bvmann>
2021-04-10 14:45:55 -0700

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,9 @@
 import os
 import csv 
+import sys 
 
 bank_path = os.path.join("Resources","pybank.csv") 
-
-
+naturalpath = sys.stdout 
 
 with open(bank_path) as bankcsv: 
  csvreader= csv.reader(bankcsv, delimiter=',')
@@ -13,27 +13,49 @@ with open(bank_path) as bankcsv:
  change = 0
  greatestincrease = []
  greatestdecrease = []
- 
+ profits_loss = []
+ main=[]
+
+   
  for x in csvreader:
-     dates=dates+(len(list(csvreader)))
-     for i in x: 
-         netprofit += int(i[1])
-         change -=int(i[1])
-         avg_change=int(change/dates)
-         greatestincrease.append(i[1])
-         greatestdecrease.append(i[1])
+   main.append(x)
+   profits_loss.append(int(x[1]))
+ dates=len(profits_loss)
+
+rate_change2=round((int(max(enumerate(profits_loss))[1])-int(min(enumerate(profits_loss))[1]))/(dates-1),2)
+
+
+netprofit = sum(profits_loss)
+
+smallchange=str(min(profits_loss))
+bigchange=str(max(profits_loss))
+
+for i in main: 
+  if i[1]==bigchange :
+     greatestincrease1 = i 
+  elif i[1]==smallchange : 
+    greatestdecrease1 = i  
      
 
-     print(min(greatestdecrease))
-     print(max(greatestincrease)) 
-     print(avg_change) 
-     print(netprofit)
-     print(dates)
-     
-   # for z in csvreader: 
-    #    change -=int(x[1])
-     #   avg_change=int(change/int(len(list(csvreader)))
-      #      print(avg_change) 
+print('Financial Analysis')
+print('---------------------')
+print("Total months: ",(dates))
+print('Total',"$",(netprofit))
+print('Average Change: ','$',(rate_change2))
+
+print('Greatest Increase in Profits: ',(greatestincrease1))
+print('Greatest Decrease in Profits: ',(greatestdecrease1))
+sys.stdout=open('analysis\_bankanalysis1.txt','w')
+
+print('Financial Analysis')
+print('---------------------')
+print("Total months: ",(dates))
+print('Total',"$",(netprofit))
+print('Average Change: ','$',(rate_change2))
+print('Greatest Increase in Profits: ',(greatestincrease1))
+print('Greatest Decrease in Profits: ',(greatestdecrease1))
+
+  
 
 
 
